f1e8ecd0488a0f50aa2c10a2b6709d86df5ee9f7
JacobusTheOne <47494589+JacobusTheOne@users.noreply.github.com>
2022-06-22 11:08:53 +0200

    My initial commit.

---
+++ Assets/Scripts/PlatformPlayer.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayer : MonoBehaviour
+{
+    public float speed = 250.0f;
+    public float jumpForce = 12.0f;
+
+    private Rigidbody2D _body;
+    private Animator _anim;
+    private BoxCollider2D _box;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+        bool grounded = false;
+        if (hit != null)
+            grounded = true;
+
+        
+        float deltaX = Input.GetAxis("Horizontal");
+        _body.velocity = new Vector2(deltaX * speed * Time.deltaTime, _body.velocity.y);
+        //Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        //_body.velocity = movement;
+        _body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+        if (Input.GetKeyDown(KeyCode.Space)&&grounded)
+        {
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        MovingPlatform platform = null;
+        if(hit != null)
+        {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+        if(platform != null)
+        {
+            transform.parent = platform.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        Vector3 pScale = Vector3.one;
+        if (platform != null)
+        {
+            pScale = platform.transform.localScale;
+        }
+        if(deltaX != 0)
+        {
+            transform.localScale = new Vector3(
+                Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
+        }
+    }
+}
